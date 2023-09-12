b62a3b3b6f49720c4180da9ad04823211ea2bb77
debashisneog23 <debashisneog23@gmail.com>
2023-09-12 23:00:24 +0530

    update logout

--- src/app/header/page.jsx
+++ src/app/header/page.jsx
@@ -65,12 +65,11 @@ const Header = () => {
 const onLogout= async ()=> {
   try {
     await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN}/api/users/logout`)
-    // 'http://localhost:3000/api/users/logout'
-
+    router.push('/login')
     toast.success('Logout Successful', {
       position: "top-center",
       });
-      router.push('/login')
+      
 
   } catch (error) {
     console.log(error);
